573a2ac8814cd4fa63651a93bd2ea0315866ca9a
codeavatar1 <bvh8924@outlook.com>
2020-07-05 08:02:29 -0500

    Initail commit

---
+++ singlerepo/src/main/java/com/rewire/SinglerepoApplication.java
@@ -0,0 +1,15 @@
+package com.rewire;
+
+import org.springframework.boot.SpringApplication;
+import java.io.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinglerepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinglerepoApplication.class, args);
+		//test commit 
+	}
+
+}
---
+++ singlerepo/src/test/java/com/rewire/SinglerepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rewire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinglerepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
